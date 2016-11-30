d0f93e43ad1a5196e6073acb158f736ac0ae3549
Sean Roger <sean.arce@intechsive.com>
2016-11-30 22:19:51 +0800

    chat-app included database

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var cool = require("cool-ascii-faces");
 var mongoClient = require("mongodb").MongoClient;
-var fetch = require("fetch");
+var bodyParser = require('body-parser');
 
-//mongoClient.connect("mongodb://root:chatapp@ds115918.mlab.com:15918/chat-app-db", function(err, database) {
-mongoClient.connect("mongodb://localhost:27017/chat-app", function(err, database) {
+mongoClient.connect("mongodb://root:chatapp@ds115918.mlab.com:15918/chat-app-db", function(err, database) {
+//mongoClient.connect("mongodb://localhost:27017/chat-app", function(err, database) {
     if (err) return console.error(err);
     const db = database;
 
     app.use("/js", express.static(path.join(__dirname, "js")));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true,
+    }));
 
     app.set("port", 5000);
 
@@ -39,5 +43,12 @@ mongoClient.connect("mongodb://localhost:27017/chat-app", function(err, database
         const test = db.collection('messages').find().toArray( function(err, results) {
             res.json(results);
         });
+    });
+
+    app.post("/save_chat", function(req, res, next) {
+        db.collection('messages').save(req.body, function(err, result) { 
+            if (err) console.error(err);
+        });
+        res.end();
     });
 });
--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@ var socket = io();
 var el = document.getElementById("form");
 var input_el = document.getElementById("m");
 var base_url = window.location.href;
+var $ = require("jquery");
+
+$.get(base_url + "fetch_all", function(data) {
+    var $url = $("#messages");
+    $.each(data, function(key, val) {
+        var $li = $("<li></li>").text(val.message);
+        $url.append($li);
+    });
+});
 
 el.addEventListener("submit", function(event) {
     event.preventDefault();
     var val = input_el.value;
+    $.post(base_url + 'save_chat', { name: 'anonymous', message: val });
     socket.emit("chat message", val);
     input_el.value = "";
     return false;
