02ec10934150af71877e9ba338496feb1a9a6869
Sean Roger <sean.arce@intechsive.com>
2016-11-30 22:50:28 +0800

    minor changes

--- js/bundle.js
+++ js/bundle.js
@@ -50,7 +50,6 @@
 	var base_url = window.location.href;
 	var $ = __webpack_require__(1);
 
-	/*
 	$.get(base_url + "fetch_all", function(data) {
 	    var $url = $("#messages");
 	    $.each(data, function(key, val) {
@@ -58,12 +57,11 @@
 	        $url.append($li);
 	    });
 	});
-	*/
 
 	el.addEventListener("submit", function(event) {
 	    event.preventDefault();
 	    var val = input_el.value;
-	    //$.post(base_url + 'save_chat', { name: 'anonymous', message: val });
+	    $.post(base_url + 'save_chat', { name: 'anonymous', message: val });
 	    socket.emit("chat message", val);
 	    input_el.value = "";
 	    return false;
--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ var input_el = document.getElementById("m");
 var base_url = window.location.href;
 var $ = require("jquery");
 
-/*
 $.get(base_url + "fetch_all", function(data) {
     var $url = $("#messages");
     $.each(data, function(key, val) {
@@ -12,12 +11,11 @@ $.get(base_url + "fetch_all", function(data) {
         $url.append($li);
     });
 });
-*/
 
 el.addEventListener("submit", function(event) {
     event.preventDefault();
     var val = input_el.value;
-    //$.post(base_url + 'save_chat', { name: 'anonymous', message: val });
+    $.post(base_url + 'save_chat', { name: 'anonymous', message: val });
     socket.emit("chat message", val);
     input_el.value = "";
     return false;
